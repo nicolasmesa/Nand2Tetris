01f283ca599a81bd132bea7cd096ccb11f17d6f2
Nicolas Mesa <nicolasmesa@gmail.com>
2016-07-23 16:58:36 -0700

    Done with project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,90 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@black
+M = -1
+
+@white
+M = 0
+
+@color
+M = 0
+
+@8192
+D = A
+
+@maxscreen
+M = D
+
+(OLOOP)
+@KBD
+D = M
+
+@SETBLACK
+D; JNE
+
+@white
+D = M
+
+@color
+M = D
+
+@ENDSETBLACK
+0; JMP
+
+(SETBLACK)
+@black
+D = M
+
+@color
+M = D
+
+(ENDSETBLACK)
+
+@i
+M = 0
+(ILOOP)
+
+@i
+D = M
+
+@maxscreen
+D = D - M
+
+@ENDILOOP
+D; JEQ
+
+@SCREEN
+D = A
+
+@i
+D = D + M
+
+@addr
+M = D
+
+@color
+D = M
+
+@addr
+A = M
+
+M = D
+
+@i
+M = M + 1
+
+@ILOOP
+0; JMP
+
+(ENDILOOP)
+
+
+@OLOOP
+0; JMP
+
+
+(END)
+@END
+0; JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// for (i = 0; i < R1; i++)
+
+@R2
+M = 0
+
+@i
+M = 0
+
+(LOOP)
+@i
+D = M;
+
+@R1
+D = D - M 
+
+@END
+D; JEQ
+
+@R0
+D = M
+
+@R2
+M = M + D 
+
+
+@i
+M = M + 1
+
+@LOOP
+0; JMP
+
+(END)
+@END
+0; JMP
